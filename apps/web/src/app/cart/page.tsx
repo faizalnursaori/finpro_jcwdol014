b39e60338774beabff590cafe328c959f2154e2d
Faizal Nursaori <faizalrahmatn@gmail.com>
2024-09-02 10:00:06 +0700

    refactor: refactor cart components

--- apps/web/src/app/cart/page.tsx
+++ apps/web/src/app/cart/page.tsx
@@ -8,11 +8,15 @@ import { ArrowLeft, Trash2 } from 'lucide-react';
 import CheckoutSummary from '@/components/CheckOutSummary';
 import WithAuth from '@/components/WithAuth';
 import { formatRupiah } from '@/utils/currencyUtils';
+import { ConfirmationDeleteCart } from '@/components/ConfirmationDeleteCart';
+import CartTable from '@/components/CartTable';
 
 const CartPage = () => {
   const { cart, fetchCart, updateItemQuantity, removeItem } = useCart();
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [itemToDelete, setItemToDelete] = useState<number | null>(null);
 
   useEffect(() => {
     setIsLoading(true);
@@ -30,10 +34,19 @@ const CartPage = () => {
   };
 
   const handleRemoveItem = async (itemId: number) => {
-    try {
-      await removeItem(itemId);
-    } catch (err) {
-      setError('Failed to remove item. Please try again.');
+    setItemToDelete(itemId);
+    setIsModalOpen(true);
+  };
+
+  const confirmDelete = async () => {
+    if (itemToDelete !== null) {
+      try {
+        await removeItem(itemToDelete);
+      } catch (err) {
+        setError('Failed to remove item. Please try again.');
+      }
+      setIsModalOpen(false);
+      setItemToDelete(null);
     }
   };
 
@@ -60,67 +73,19 @@ const CartPage = () => {
           </Link>
         </div>
       </div>
-      <table className="w-full">
-        <thead>
-          <tr className="border-b">
-            <th className="text-left py-2 text-xs">PRODUCT</th>
-            <th className="text-left py-2 text-xs">PRICE</th>
-            <th className="text-left py-2 text-xs">QUANTITY</th>
-            <th className="text-left py-2 text-xs">TOTAL</th>
-            <th className="text-left py-2 text-xs"></th>
-          </tr>
-        </thead>
-        <tbody>
-          {cart.items.map((item) => (
-            <tr key={item.id} className="border-b">
-              <td className="py-4">
-                <div className="flex items-center">
-                  <span>{item.product.name}</span>
-                </div>
-              </td>
-              <td className="py-4">{formatRupiah(item.product.price)}</td>
-              <td className="py-4">
-                <div className="flex items-center">
-                  <button
-                    onClick={() =>
-                      handleUpdateQuantity(
-                        item.id,
-                        Math.max(1, item.quantity - 1),
-                      )
-                    }
-                    className="px-[11px] py-1 bg-gray-200 rounded-full"
-                  >
-                    -
-                  </button>
-                  <span className="mx-2">{item.quantity}</span>
-                  <button
-                    onClick={() =>
-                      handleUpdateQuantity(item.id, item.quantity + 1)
-                    }
-                    className="px-[10px] py-1 bg-gray-200 rounded-full"
-                  >
-                    +
-                  </button>
-                </div>
-              </td>
-              <td className="py-4">
-                {formatRupiah(item.product.price * item.quantity)}
-              </td>
-              <td className="py-4">
-                <button
-                  onClick={() => handleRemoveItem(item.id)}
-                  className="text-red-500"
-                >
-                  <Trash2 size={20} />
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <CartTable
+        items={cart.items}
+        onUpdateQuantity={handleUpdateQuantity}
+        onRemoveItem={handleRemoveItem}
+      />
       <div className="md:w-1/3 ml-auto">
         <CheckoutSummary />
       </div>
+      <ConfirmationDeleteCart
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onConfirm={confirmDelete}
+      />
     </div>
   );
 };
